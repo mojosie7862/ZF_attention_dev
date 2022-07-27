95601192ba990a04c55f0e39178d35ef7fe0995e
jo <jem56@uw.edu>
2022-07-27 11:30:36 -0400

    camera troubleshooting

--- stitch_cv_ppt.py
+++ stitch_cv_ppt.py
@@ -1,4 +1,3 @@
-from util import *
 import random
 import tkinter
 import cv2
@@ -23,7 +22,7 @@ pre_stimulus_time = 4
 pre_reward_time = 4
 reward_aversion_time = 10
 
-filename = r'C:\Users\josep\anaconda3\envs\ZF_attention_project\paradigms.pptx'
+filename = r'C:\Users\Kanwal\Dropbox\Josephine Zfish\ZF_attention\paradigms.pptx'
 
 class VideoRecorder():
 
@@ -31,13 +30,13 @@ class VideoRecorder():
     def __init__(self, run, paradigm):
 
         self.open = True
-        self.device_index = 1
-        self.fps = 6  # fps should be the minimum constant rate at which the camera can
-        self.fourcc = "MJPG"  # capture images (with no decrease in speed over time; testing is required)
+        self.device_index = 0
+        self.fps = 60  # fps should be the minimum constant rate at which the camera can
+        #self.fourcc = "MJPG"  # capture images (with no decrease in speed over time; testing is required)
         self.frameSize = (640, 480)  # video formats and sizes also depend and vary according to the camera used
         self.video_filename = fishID + "_run_" + str(run) + "_" + paradigm + ".avi"
         self.video_cap = cv2.VideoCapture(self.device_index)
-        self.video_writer = cv2.VideoWriter_fourcc(*self.fourcc)
+        self.video_writer = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
         self.video_out = cv2.VideoWriter(self.video_filename, self.video_writer, self.fps, self.frameSize)
         self.frame_counts = 1
         self.start_time = time.time()
