a8a8b7b17e043c4f5e5fdaa238b6e0d564947f79
jo <jem56@uw.edu>
2022-07-28 12:43:52 -0400

    final pres with UI

--- Final_Paradigm.py
+++ Final_Paradigm.py
@@ -6,21 +6,28 @@ import time
 import os
 import win32com.client
 import win32api
+import pythoncom
 
-user_initials = "JM"
-numruns = 6
+camwidth = 640
+camheight = 480
+toggle = 1;
+recordingIndex = -999
+
+cam_id = 0
+user_initial = "SM"
+
+numruns = 3
 mindelay = 3
 maxdelay = 5
 
-fishID = "Zpilot"
-gender = ""
-genotype = "WT"
+zfish_id = "Z1"
+gender = "(blank)"
+genotype = "(blank)"
 notes = "(blank)"
-fishID = fishID+gender+genotype
 
 pre_stimulus_time = 4
 pre_reward_time = 4
-reward_aversion_time = 10
+reward_aversion_time = 4
 
 filename = r'C:\Users\Kanwal\Dropbox\Josephine Zfish\ZF_attention\paradigms.pptx'
 
@@ -34,7 +41,7 @@ class VideoRecorder():
         self.fps = 20  # fps should be the minimum constant rate at which the camera can
         self.fourcc = "XVID"  # capture images (with no decrease in speed over time; testing is required)
         self.frameSize = (640, 480)  # video formats and sizes also depend and vary according to the camera used
-        self.video_filename = fishID + "_run_" + str(run) + "_" + paradigm + ".avi"
+        self.video_filename = zfish_id + "_run_" + str(run) + "_" + paradigm + ".avi"
         self.video_cap = cv2.VideoCapture(self.device_index)
         self.video_writer = cv2.VideoWriter_fourcc(*self.fourcc)
         self.video_out = cv2.VideoWriter(self.video_filename, self.video_writer, self.fps, self.frameSize)
@@ -85,7 +92,7 @@ def start_PPTrecording(filename):
     all_runs = [['cf', 0], ['dfm', 0], ['ufm', 0]]
     global fixed_times
     fixed_times = [1, 1000, 1, 2000, 1]
-
+    pythoncom.CoInitialize()
     app = win32com.client.Dispatch("PowerPoint.Application")
     app.Visible = 1
     app.Presentations.Open(FileName=filename)
@@ -97,9 +104,6 @@ def start_PPTrecording(filename):
     print("nt thread opened")
     time.sleep(10) #change to 360 for true trials
     novtest_vthread.stop()
-    while threading.active_count() > 1:
-        print("closing thread")
-        time.sleep(1)
     print("nt thread closed")
 
     #loop through paradigm presentations and record from pre-stimulus to post reward/aversion
@@ -113,6 +117,7 @@ def start_PPTrecording(filename):
         video_thread.start()
 
         win32api.Sleep((pre_stimulus_time * 1000) + 2000)  # pre-stimulus time
+        print(this_run[1])
         app.SlideShowWindows(1).View.GotoSlide(this_run[1])  # advance to screen cue
         win32api.Sleep(fixed_times[0])  # fixed 1
         app.SlideShowWindows(1).View.Next()  # play screen cue
@@ -139,14 +144,21 @@ def start_PPTrecording(filename):
         time.sleep(iti)
         video_thread.stop()
         print("thread stopped")
-        while threading.active_count() > 1:
-            print("closing thread")
-            time.sleep(1)
         if len(all_runs) == 0:
             app.SlideShowWindows(1).View.GotoSlide(1)
+            pythoncom.CoUninitialize()
             print("Presentation finished.")
+            with open("transcript.txt","a+") as wfile:
+                wfile.write("Presentation finished.\n")
             break
+        with open("transcript.txt","a+") as wfile:
+            wfile.write("Filename: "+video_thread.video_filename+"\n\n")
 
+
+def main_():
+    import win32com.client
+    import win32api
+    start_PPTrecording(filename)
 
 def tkinter_start():
     top = tkinter.Tk()
@@ -154,227 +166,239 @@ def tkinter_start():
     def action():
         global toggle
         toggle *= -1
-        if (toggle == -1):
-            with open("transcript.txt", "a+") as wfile:
+        if(toggle==-1):
+            with open("transcript.txt","a+") as wfile:
                 wfile.write("Emergency stop")
             print("Stopped Recording, exiting program")
             cv2.destroyAllWindows()
             os._exit(0)
 
-    B = tkinter.Button(top, text="Stop Recording", command=action)
+    B = tkinter.Button(top, text ="Stop Recording", command = action)
 
     B.pack()
     top.mainloop()
 
+def supermain():
+    t1 = threading.Thread(target=main_)
+    t2 = threading.Thread(target=tkinter_start)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
 def startup():
+    
     top1 = tkinter.Tk()
     top1.title("Zfish Interface")
     mainpanel = tkinter.PanedWindow(orient=tkinter.VERTICAL)
-    mainpanel.pack(fill=tkinter.BOTH, expand=1)
+    mainpanel.pack(fill=tkinter.BOTH,expand = 1)
     panel1 = tkinter.PanedWindow(mainpanel)
     panel1.pack(fill=tkinter.BOTH, expand=1)
 
-    top1.geometry('368x329')
+    top1.geometry('368x340')    
+    def c():  
+        if(os.path.exists("transcript.txt")):   
+            os.remove("transcript.txt")   
+        with open("transcript.txt", "a+") as wfile:   
+            import datetime  
+            wfile.write("DateTime: "+str(datetime.datetime.now())+"\n")  
 
-    def c():
-        if (os.path.exists("transcript.txt")):
-            os.remove("transcript.txt")
-        with open("transcript.txt", "a+") as wfile:
-            import datetime
-            wfile.write("DateTime: " + str(datetime.datetime.now()) + "\n")
-
-            # global cam_id
-            # if (not (txt.get() == "")):
-            #     cam_id = int(txt.get())
-            #     wfile.write("CameraID: " + str(cam_id) + "\n")
-            # else:
-            #     wfile.write("CameraID: 0 DEFAULT\n")
-
-            global user_initials
-            user_initials = txt3.get()
-            if (not (txt3.get() == "")):
-                wfile.write("User Initials: " + str(user_initials) + "\n")
+            global cam_id    
+            if(not(txt.get()=="")):    
+                cam_id = int(txt.get())    
+                wfile.write("CameraID: "+str(cam_id)+"\n")  
+            else:   
+                wfile.write("CameraID: 0 DEFAULT\n") 
+            global user_initial
+            user_initial = txt3.get()
+            if(not(txt3.get()=="")):
+                wfile.write("User Initials: "+str(user_initial)+"\n")
             else:
-                wfile.write("User Initials: (noname)\n")
-
+                wfile.write("User Initials: (blank)\n")
             global numruns
-            if (not (txt4.get() == "")):
+            if(not(txt4.get()=="")):
                 numruns = int(txt4.get())
-                wfile.write("Number of Runs/Recordings: " + str(numruns) + "\n")
+                wfile.write("Number of Runs/Recordings: "+str(numruns)+"\n")
             else:
-                wfile.write("Number of Runs/Recordings: 1 DEFAULT\n")
+                wfile.write("Number of Runs/Recordings: 3 DEFAULT\n")
 
             global mindelay
-            if (not (txt5.get() == "")):
+            if(not(txt5.get()=="")):
                 mindelay = int(txt5.get())
-                wfile.write("Min ITI: " + str(mindelay) + " seconds\n")
+                wfile.write("Min ITI: "+str(mindelay)+" seconds\n")
             else:
-                wfile.write("Min ITI: 5 seconds DEFAULT\n")
+                wfile.write("Min ITI: 3 seconds DEFAULT\n")
 
             global maxdelay
-            if (not (txt51.get() == "")):
+            if(not(txt51.get()=="")):
                 maxdelay = int(txt51.get())
-                wfile.write("Max ITI: " + str(maxdelay) + " seconds\n")
+                wfile.write("Max ITI: "+str(maxdelay)+" seconds\n")
             else:
-                wfile.write("Max ITI: 10 seconds DEFAULT\n")
+                wfile.write("Max ITI: 5 seconds DEFAULT\n")
+
 
             global pre_stimulus_time
-            if (not (txt52.get() == "")):
+            if(not(txt52.get()=="")):
                 pre_stimulus_time = int(txt52.get())
-                wfile.write("Pre-Stimulus Time: " + str(pre_stimulus_time) + " seconds\n")
+                wfile.write("Pre-stimulus Time: "+str(pre_stimulus_time)+"\n")
             else:
-                wfile.write("Pre-Stimulus Time: (blank)\n")
+                wfile.write("Pre_stimulus Time: (blank)\n")
 
             global pre_reward_time
-            if (not (txt53.get() == "")):
+            if(not(txt53.get()=="")):
                 pre_reward_time = int(txt53.get())
-                wfile.write("Pre-Reward Time: " + str(pre_reward_time) + " seconds\n")
+                wfile.write("Pre-reward Time: "+str(pre_reward_time)+"\n")
             else:
-                wfile.write("Pre-Reward Time: (blank)\n")
+                wfile.write("Pre-reward Time: (blank)\n")
 
             global reward_aversion_time
-            if (not (txt54.get() == "")):
+            if(not(txt54.get()=="")):
                 reward_aversion_time = int(txt54.get())
-                wfile.write("Reward/Aversion Time: " + str(reward_aversion_time) + " seconds\n")
+                wfile.write("Reward/Aversion Time: "+str(reward_aversion_time)+"\n")
             else:
                 wfile.write("Reward/Aversion Time: (blank)\n")
 
-            global fishID
-            fishID = txt7.get()
-            if (not (txt7.get() == "")):
-                wfile.write("Fish ID: " + str(fishID) + "\n")
+
+            global zfish_id 
+            if(not(txt7.get()=="")):
+                zfish_id  = txt7.get()
+                wfile.write("Zfish ID: "+str(zfish_id )+"\n")
             else:
-                wfile.write("Fish ID: (noname)\n")
+                wfile.write("Zfish ID: Z1 DEFAULT\n")           
 
             global gender
-            gender = txt8.get()
-            if (not (txt8.get() == "")):
-                wfile.write("Gender (M/F): " + str(gender) + "\n")
+            if(not(txt8.get()=="")):
+                gender = txt8.get()
+            
+                wfile.write("Gender (M/F): "+str(gender)+"\n")
             else:
-                wfile.write("Gender (M/F): (blank)\n")
+                wfile.write("Gender (M/F): (blank)\n")  
 
-            global status
-            status = txt9.get()
-            if (not (txt9.get() == "")):
-                wfile.write("Status (W/M/T): " + str(status) + "\n")
+            global genotype
+            if(not(txt9.get()=="")):
+                genotype = txt9.get()
+            
+                wfile.write("Genotype (W/M/T): "+str(genotype)+"\n")
             else:
-                wfile.write("Status (W/M/T): (blank)\n")
+                wfile.write("Genotype (W/M/T): (blank)\n")
 
             global notes
-            notes = txt10.get()
-            if (not (txt10.get() == "")):
-                wfile.write("Notes: " + str(notes) + "\n")
+            if(not(txt10.get()=="")):
+                notes = txt10.get()
+            
+                wfile.write("Notes: "+str(notes)+"\n")
             else:
-                wfile.write("Notes: (nonotes)\n")
-
+                wfile.write("Notes: (blank)\n")
+            
             top1.destroy()
-            wfile.write("-" * 50 + "\n")
+            wfile.write("-"*50+"\n")
             return 1
-
     def val(char):
         if str.isdigit(char) or char == "":
             return True
         else:
             return False
 
-    #val1 = (top1.register(val))
+    val1 = (top1.register(val))
     val2 = (top1.register(val))
 
-    # txt = tkinter.Entry(top1, validate='all', validatecommand=(val1, '%P'))
-    #
-    # label = tkinter.Label(top1, text="Cam Id:")
-    #
-    # panel1.add(label)
-    # panel1.add(txt)
-    panel2 = tkinter.PanedWindow(mainpanel, orient=tkinter.VERTICAL)
+    txt = tkinter.Entry(top1, validate='all', validatecommand=(val1, '%P'))
 
+    label = tkinter.Label(top1, text="Cam Id:")
+    
+    panel1.add(label)
+    panel1.add(txt)
+
+    panel2 = tkinter.PanedWindow(mainpanel,orient=tkinter.VERTICAL)
     panel2.pack()
 
-    panel3 = tkinter.PanedWindow(panel2, orient=tkinter.HORIZONTAL)
+    panel3 = tkinter.PanedWindow(panel2,orient=tkinter.HORIZONTAL)
     panel3.pack()
     label3 = tkinter.Label(top1, text="User Initials: ")
     panel3.add(label3)
-    txt3 = tkinter.Entry(top1, validate='all')
+    txt3 = tkinter.Entry(top1, validate='all') 
     panel3.add(txt3)
 
-    panel4 = tkinter.PanedWindow(panel2, orient=tkinter.HORIZONTAL)
+    panel4 = tkinter.PanedWindow(panel2,orient=tkinter.HORIZONTAL)
     panel4.pack()
     label4 = tkinter.Label(top1, text="Number of Runs/Recordings: ")
     panel4.add(label4)
-    txt4 = tkinter.Entry(top1, validate='all', validatecommand=(val2, '%P'))
+    txt4 = tkinter.Entry(top1, validate='all', validatecommand=(val2, '%P')) 
     panel4.add(txt4)
 
-    panel5 = tkinter.PanedWindow(panel2, orient=tkinter.HORIZONTAL)
+    panel5 = tkinter.PanedWindow(panel2,orient=tkinter.HORIZONTAL)
     panel5.pack()
     label5 = tkinter.Label(top1, text="ITI min (sec): ")
     panel5.add(label5)
-    txt5 = tkinter.Entry(top1, validate='all', validatecommand=(val2, '%P'))
+    txt5 = tkinter.Entry(top1, validate='all', validatecommand=(val2, '%P')) 
     panel5.add(txt5)
 
-    panel51 = tkinter.PanedWindow(panel2, orient=tkinter.HORIZONTAL)
+    panel51 = tkinter.PanedWindow(panel2,orient=tkinter.HORIZONTAL)
     panel51.pack()
     label51 = tkinter.Label(top1, text="ITI max (sec): ")
     panel51.add(label51)
-    txt51 = tkinter.Entry(top1, validate='all', validatecommand=(val2, '%P'))
+    txt51 = tkinter.Entry(top1, validate='all', validatecommand=(val2, '%P')) 
     panel51.add(txt51)
-
-    panel52 = tkinter.PanedWindow(panel2, orient=tkinter.HORIZONTAL)
+    
+    panel52 = tkinter.PanedWindow(panel2,orient=tkinter.HORIZONTAL)
     panel52.pack()
-    label52 = tkinter.Label(top1, text="Pre-Stimulus Time: ")
+    label52 = tkinter.Label(top1, text="Pre_stimulus Time: ")
     panel52.add(label52)
-    txt52 = tkinter.Entry(top1, validate='all')
+    txt52 = tkinter.Entry(top1, validate='all') 
     panel52.add(txt52)
 
-    panel53 = tkinter.PanedWindow(panel2, orient=tkinter.HORIZONTAL)
+    panel53 = tkinter.PanedWindow(panel2,orient=tkinter.HORIZONTAL)
     panel53.pack()
-    label53 = tkinter.Label(top1, text="Pre-Reward Time: ")
+    label53 = tkinter.Label(top1, text="Pre-reward Time: ")
     panel53.add(label53)
-    txt53 = tkinter.Entry(top1, validate='all')
+    txt53 = tkinter.Entry(top1, validate='all') 
     panel53.add(txt53)
 
-    panel54 = tkinter.PanedWindow(panel2, orient=tkinter.HORIZONTAL)
+    panel54 = tkinter.PanedWindow(panel2,orient=tkinter.HORIZONTAL)
     panel54.pack()
     label54 = tkinter.Label(top1, text="Reward/Aversion Time: ")
     panel54.add(label54)
-    txt54 = tkinter.Entry(top1, validate='all', )
+    txt54 = tkinter.Entry(top1, validate='all',) 
     panel54.add(txt54)
 
-    panel6 = tkinter.PanedWindow(panel2, orient=tkinter.VERTICAL)
+    panel6 = tkinter.PanedWindow(panel2,orient=tkinter.VERTICAL)
     panel6.pack()
     label6 = tkinter.Label(top1, text="Fish Information: ")
     label6.config(font=("Courier", 24))
     panel6.add(label6)
 
-    panel7 = tkinter.PanedWindow(panel2, orient=tkinter.HORIZONTAL)
+    panel7 = tkinter.PanedWindow(panel2,orient=tkinter.HORIZONTAL)
     panel7.pack()
-    label7 = tkinter.Label(top1, text="Fish ID: ")
+    label7 = tkinter.Label(top1, text="Zfish ID: ")
     panel7.add(label7)
-    txt7 = tkinter.Entry(top1, validate='all')
+    txt7 = tkinter.Entry(top1, validate='all') 
     panel7.add(txt7)
 
-    panel8 = tkinter.PanedWindow(panel2, orient=tkinter.HORIZONTAL)
+    panel8 = tkinter.PanedWindow(panel2,orient=tkinter.HORIZONTAL)
     panel8.pack()
     label8 = tkinter.Label(top1, text="Gender (M/F): ")
     panel8.add(label8)
-    txt8 = tkinter.Entry(top1, validate='all')
+    txt8 = tkinter.Entry(top1, validate='all') 
     panel8.add(txt8)
 
-    panel9 = tkinter.PanedWindow(panel2, orient=tkinter.HORIZONTAL)
+    panel9 = tkinter.PanedWindow(panel2,orient=tkinter.HORIZONTAL)
     panel9.pack()
-    label9 = tkinter.Label(top1, text="Status (W/M/T): ")
+    label9 = tkinter.Label(top1, text="Genotype (W/M/T): ")
     panel9.add(label9)
-    txt9 = tkinter.Entry(top1, validate='all')
+    txt9 = tkinter.Entry(top1, validate='all') 
     panel9.add(txt9)
 
-    panel10 = tkinter.PanedWindow(panel2, orient=tkinter.HORIZONTAL)
+    panel10 = tkinter.PanedWindow(panel2,orient=tkinter.HORIZONTAL)
     panel10.pack()
     label10 = tkinter.Label(top1, text="Notes: ")
     panel10.add(label10)
-    txt10 = tkinter.Entry(top1, validate='all')
+    txt10 = tkinter.Entry(top1, validate='all') 
     panel10.add(txt10)
 
-    C = tkinter.Button(top1, text="GO", command=c)
-    C.pack(side=tkinter.BOTTOM)
+
+    C = tkinter.Button(top1, text ="GO", command = c)
+    C.pack(side = tkinter.BOTTOM)
     top1.mainloop()
 
-start_PPTrecording(filename)
+startup()
+supermain()
